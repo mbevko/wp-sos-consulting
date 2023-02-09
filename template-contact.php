5794dbda01f8ef121d3129fb5342d17d1ff79bdf
Michael Evko <mbevko@gmail.com>
2023-02-09 16:42:24 -0500

    added contact page to php

---
+++ template-contact.php
@@ -0,0 +1,20 @@
+<?php
+/*
+Template Name: Contact
+*/
+?>
+
+<?php get_header();?>
+
+
+<section class="more_info_body">
+            <div class="white_space">
+                <h2 class="subject_title"><?php the_title();?></h2>
+               
+                <div class="back_banner"><h2>Send Us a Message</h2></div>
+            </div>
+            <div class="article"></div>
+    </section>
+
+<?php get_footer();?>
+
