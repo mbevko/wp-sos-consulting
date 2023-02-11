9214ab9cb951c65daab0118cef38bc1b215ec4d3
Michael Evko <mbevko@gmail.com>
2023-02-11 01:41:14 -0500

    coded contact page basic structure and design

--- template-contact.php
+++ template-contact.php
@@ -7,13 +7,35 @@ Template Name: Contact
 <?php get_header();?>
 
 
-<section class="more_info_body">
-            <div class="white_space">
-                <h2 class="subject_title"><?php the_title();?></h2>
-               
-                <div class="back_banner"><h2>Send Us a Message</h2></div>
+    <section class="contact_body">
+            <div class="contact_white_space">
+                
+                <div class="back_banner">
+                    <div class="contact_container_left">
+                        <div class="contact_info_left">
+                            <img class="contact_page_icons" src="<?php echo get_template_directory_uri(); ?>/images/phone.png">
+                            <img class="contact_page_icons" src="<?php echo get_template_directory_uri(); ?>/images/mail.png">
+                            <img class="contact_page_icons" src="<?php echo get_template_directory_uri(); ?>/images/linkedin.png">
+                        </div>
+                        <div class="contact_info_right">
+                            <p>555.987.6543</p>
+                            <p>info@sosconsultingservices.net</p>
+                            <p>linkedin.com/sosconsultingservices</p>   
+                        </div>  
+                    </div>
+                    <div class="contact_container_right">
+                        <h2 class="contact_title"><?php the_title();?></h2>
+                        <form class="contact_form">
+                            <input type="text" name="name" class="name" placeholder="Name"/>
+                            <input type="text" name="comp_name" class="comp_name"  placeholder="Company Name"/>
+                            <input type="text" name="contact_email" class="contact_email"  placeholder="Email"/>
+                            <input type="text" name="contact_phone" class="contact_phone"  placeholder="Phone"/>
+                            <textarea name="message" class="message" placeholder="Write us a Message"></textarea>
+                            <button class="form_submit">Send Message</button>
+                        </form>
+                    </div>
+                </div>
             </div>
-            <div class="article"></div>
     </section>
 
 <?php get_footer();?>
