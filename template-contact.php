772927fc3e29c67500764c000c2dc2a5e16fac26
Michael Evko <mbevko@gmail.com>
2024-10-11 09:07:35 -0500

    made changes to mobile responsivness on home page, on contact pop up forms, and on contact page

--- template-contact.php
+++ template-contact.php
@@ -7,44 +7,45 @@ Template Name: Contact
 <?php get_header();?>
 
 <section class="contact_body">
-
-    <div class="back_banner">
-        <div class="contact_container_left">
-            <p class="page_info_headings">Call Us</p>
-            <a href="tel:18442448869" class="contact_page_info">
-                <img
-                    class="contact_page_icons"
-                    src="<?php echo get_template_directory_uri(); ?>/images/phone.png"/>
+    <div class="contact_body_container">
+        <div class="back_banner">
+            <div class="contact_container_left">
+                <p class="page_info_headings">Call Us</p>
+                <a href="tel:18442448869" class="contact_page_info">
+                    <img
+                        class="contact_page_icons"
+                        src="<?php echo get_template_directory_uri(); ?>/images/phone.png"/>
                     +1 (844)-244-8869
-            </a>
-            <p class="page_info_headings">Email</p>
-            <a href="mailto:info@sosconsultingservices.net" class="contact_page_info">
-                <img
-                    class="contact_page_icons"
-                    src="<?php echo get_template_directory_uri(); ?>/images/mail.png"/>
-                <p>info@sosconsultingservices.net</p>
-            </a>
-            <p class="page_info_headings">Linkedin</p>
-            <a
-                href="https://www.linkedin.com/company/sos-consulting-services-inc-"
-                class="contact_page_info">
-                <img
-                    class="contact_page_icons"
-                    src="<?php echo get_template_directory_uri(); ?>/images/linkedin.png"/>
-                <p>linkedin.com/sosconsultingservices</p>
-            </a>
-        </div>
-        <div class="contact_container_right">
-            <h2 class="contact_title"><?php the_title();?></h2>
-            <script
-                charset="utf-8"
-                type="text/javascript"
-                src="//js.hsforms.net/forms/embed/v2.js"></script>
-            <script>
-                hbspt
-                    .forms
-                    .create({region: "na1", portalId: "45057611", formId: "af3805f1-bf73-41ad-b69a-b803e4578b0b"});
-            </script>
+                </a>
+                <p class="page_info_headings">Email</p>
+                <a href="mailto:info@sosconsultingservices.net" class="contact_page_info">
+                    <img
+                        class="contact_page_icons"
+                        src="<?php echo get_template_directory_uri(); ?>/images/mail.png"/>
+                    <p>info@sosconsultingservices.net</p>
+                </a>
+                <p class="page_info_headings">Linkedin</p>
+                <a
+                    href="https://www.linkedin.com/company/sos-consulting-services-inc-"
+                    class="contact_page_info">
+                    <img
+                        class="contact_page_icons"
+                        src="<?php echo get_template_directory_uri(); ?>/images/linkedin.png"/>
+                    <p>linkedin.com/sosconsultingservices</p>
+                </a>
+            </div>
+            <div class="contact_container_right">
+                <h2 class="contact_title"><?php the_title();?></h2>
+                <script
+                    charset="utf-8"
+                    type="text/javascript"
+                    src="//js.hsforms.net/forms/embed/v2.js"></script>
+                <script>
+                    hbspt
+                        .forms
+                        .create({region: "na1", portalId: "45057611", formId: "af3805f1-bf73-41ad-b69a-b803e4578b0b"});
+                </script>
+            </div>
         </div>
     </div>
 
